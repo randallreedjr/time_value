20378913c1640a316bdfc0c3c7473680b82ea5ec
Randall Reed, Jr <randallreedjr@gmail.com>
2013-12-22 00:13:25 -0500

    calcN working

---
+++ TimeValue.rb
@@ -0,0 +1,37 @@
+class TimeValue
+    
+    protected :n, :i, :pv, :pmt, :fv
+    
+    def initialize(n, i, pv, pmt, fv)
+        @n = n
+        @i = i
+        @pv = pv
+        @pmt = pmt
+        @fv = fv
+    end
+    
+    def calcPV()
+        i = @i / 100.0
+        pvf = @fv / ((1 + i) ** @n)
+        pva = (@pmt/i) * (1-(1/((1+i)**@n)))
+        @pv = (pvf + pva) * (-1)
+        #Round
+        @pv = (@pv*100).round / 100.0
+    end
+    
+    def calcFV()
+        i = @i / 100.0
+        #Growth of initial contribution
+        fvp = @pv * ((1 + i) ** @n)
+        #Growth of payments
+        fva = @pmt * (((1 + i) ** @n)-1) / i
+        @fv = (fvp + fva) * (-1)
+        #Round
+        @fv = (@fv*100).round / 100.0
+    end
+    
+    def calcN()
+        i = @i / 100.0
+        @n = (Math.log((@pmt - (i * @fv))/(@pmt + (i * @pv)))/Math.log(1 + i))
+    end
+end
---
+++ test.rb
@@ -0,0 +1,2 @@
+x = TimeValue.new(0,8,0,-100,5000)
+x.calcN
