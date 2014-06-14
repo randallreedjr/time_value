2cc7e29510b92d198e59e8102011a1fd3d535f1c
randallreedjr <randallreedjr@gmail.com>
2014-06-13 21:31:27 -0400

    Update spec to use environment file

--- spec/timevalue_spec.rb
+++ spec/timevalue_spec.rb
@@ -1,9 +1,9 @@
-require_relative 'spec_helper.rb'
+require_relative '../config/environment.rb'
 
 describe 'timevalue' do
   it 'should be initialized with 0 as defaults' do
     time_value = TimeValue.new()
     expect(time_value.n).to eq(0)
-    
+
   end
 end
