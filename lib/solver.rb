a4645b3913951b7242ca671cb8627eb91b288f08
Randall Reed, Jr <randallreedjr@gmail.com>
2016-07-31 15:22:07 -0700

    Adjust precision and iterations

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,4 +1,7 @@
 class Solver
+  PRECISION = 3
+  INTERVAL = 10 ** (-1 * PRECISION)
+  MAX_ITERATIONS = 20
   attr_reader :time_value, :goal
   attr_accessor :guess, :lower_bound, :upper_bound, :upper_cap_met
 
@@ -14,7 +17,8 @@ class Solver
   def solve!
     iteration_count = 0
     time_value.i = guess
-    while (upper_bound - lower_bound).round(2) > 0.01 && iteration_count < 100
+    while (upper_bound - lower_bound).round(PRECISION) > INTERVAL &&
+      iteration_count < MAX_ITERATIONS
       iteration_count += 1
       begin
         result = time_value.calc_fv
@@ -24,7 +28,7 @@ class Solver
       adjust_bounds(result)
       time_value.i = guess
     end
-    guess if iteration_count < 100
+    guess.round(2) if iteration_count < MAX_ITERATIONS
   end
 
   def adjust_bounds(result)
@@ -37,6 +41,6 @@ class Solver
       self.upper_bound *= 2 unless upper_cap_met
       self.lower_bound = guess
     end
-    self.guess = ((upper_bound + lower_bound) / 2).round(2)
+    self.guess = ((upper_bound + lower_bound) / 2).round(PRECISION)
   end
 end
